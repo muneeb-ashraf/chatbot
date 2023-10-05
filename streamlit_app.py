5908df37a2d547c6f7309e2533b7e0e72d78d8a9
Muneeb Ashraf <58017595+muneebashraf987@users.noreply.github.com>
2023-10-05 22:26:58 +0500

    header and footer change

--- streamlit_app.py
+++ streamlit_app.py
@@ -119,6 +119,8 @@ hide_streamlit_style = """
             <style>
             #MainMenu {visibility: hidden;}
             footer {visibility: hidden;}
+            .viewerBadge_container__r5tak {visibility:hidden;}
+            header {visibility: hidden;}
             </style>
             """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
