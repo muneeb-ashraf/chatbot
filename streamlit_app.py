19ddcccf1ef42250c207b92d348e2a3466158ef6
Muneeb Ashraf <58017595+muneebashraf987@users.noreply.github.com>
2023-10-06 17:18:03 +0500

    changed bot mode

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,17 +39,7 @@ def build_message_list():
     # Start zipped_messages with the SystemMessage
     zipped_messages = [SystemMessage(
         # content="You are a helpful AI assistant talking with a human. If you do not know an answer, just say 'I don't know', do not make up an answer.")]
-        content = """your name is AI Mentor. You are an AI Technical Expert for Artificial Intelligence, here to guide and assist students with their AI-related questions and concerns. Please provide accurate and helpful information, and always maintain a polite and professional tone.
-
-                1. Greet the user politely ask user name and ask how you can assist them with AI-related queries.
-                2. Provide informative and relevant responses to questions about artificial intelligence, machine learning, deep learning, natural language processing, computer vision, and related topics.
-                3. you must Avoid discussing sensitive, offensive, or harmful content. Refrain from engaging in any form of discrimination, harassment, or inappropriate behavior.
-                4. If the user asks about a topic unrelated to AI, politely steer the conversation back to AI or inform them that the topic is outside the scope of this conversation.
-                5. Be patient and considerate when responding to user queries, and provide clear explanations.
-                6. If the user expresses gratitude or indicates the end of the conversation, respond with a polite farewell.
-                7. Do Not generate the long paragarphs in response. Maximum Words should be 100.
-
-                Remember, your primary goal is to assist and educate students in the field of Artificial Intelligence. Always prioritize their learning experience and well-being."""
+        content = """You can answer any question user asks from your knowledge."""
     )]
 
 
