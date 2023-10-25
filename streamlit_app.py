94ea952e36767f5d9d39847965ebbdbce44be988
Muneeb Ashraf <58017595+muneebashraf987@users.noreply.github.com>
2023-10-26 02:01:45 +0500

    new app

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,118 +1,124 @@
-from itertools import zip_longest
+from dataclasses import dataclass
+from typing import Literal
 import streamlit as st
-from streamlit_chat import message
-from langchain.chat_models import ChatOpenAI
-from langchain.schema import (
-    SystemMessage,
-    HumanMessage,
-    AIMessage
+
+from langchain import OpenAI
+from langchain.callbacks import get_openai_callback
+from langchain.chains import ConversationChain
+from langchain.chains.conversation.memory import ConversationSummaryMemory
+import streamlit.components.v1 as components
+
+@dataclass
+class Message:
+    """Class for keeping track of a chat message."""
+    origin: Literal["human", "ai"]
+    message: str
+
+def load_css():
+    with open("static/styles.css", "r") as f:
+        css = f"<style>{f.read()}</style>"
+        st.markdown(css, unsafe_allow_html=True)
+
+def initialize_session_state():
+    if "history" not in st.session_state:
+        st.session_state.history = []
+    if "token_count" not in st.session_state:
+        st.session_state.token_count = 0
+    if "conversation" not in st.session_state:
+        llm = OpenAI(
+            temperature=0,
+            openai_api_key=st.secrets["OPENAI_API_KEY"],
+            model_name="text-davinci-003"
+        )
+        st.session_state.conversation = ConversationChain(
+            llm=llm,
+            memory=ConversationSummaryMemory(llm=llm),
+        )
+
+def on_click_callback():
+    with get_openai_callback() as cb:
+        human_prompt = st.session_state.human_prompt
+        llm_response = st.session_state.conversation.run(
+            human_prompt
+        )
+        st.session_state.history.append(
+            Message("human", human_prompt)
+        )
+        st.session_state.history.append(
+            Message("ai", llm_response)
+        )
+        st.session_state.token_count += cb.total_tokens
+
+load_css()
+initialize_session_state()
+
+st.title("Hello Custom CSS Chatbot 🤖")
+
+chat_placeholder = st.container()
+prompt_placeholder = st.form("chat-form")
+credit_card_placeholder = st.empty()
+
+with chat_placeholder:
+    for chat in st.session_state.history:
+        div = f"""
+<div class="chat-row 
+    {'' if chat.origin == 'ai' else 'row-reverse'}">
+    <img class="chat-icon" src="app/static/{
+        'ai_icon.png' if chat.origin == 'ai' 
+                      else 'user_icon.png'}"
+         width=32 height=32>
+    <div class="chat-bubble
+    {'ai-bubble' if chat.origin == 'ai' else 'human-bubble'}">
+        &#8203;{chat.message}
+    </div>
+</div>
+        """
+        st.markdown(div, unsafe_allow_html=True)
+    
+    for _ in range(3):
+        st.markdown("")
+
+with prompt_placeholder:
+    st.markdown("**Chat**")
+    cols = st.columns((6, 1))
+    cols[0].text_input(
+        "Chat",
+        value="Hello bot",
+        label_visibility="collapsed",
+        key="human_prompt",
+    )
+    cols[1].form_submit_button(
+        "Submit", 
+        type="primary", 
+        on_click=on_click_callback, 
+    )
+
+credit_card_placeholder.caption(f"""
+Used {st.session_state.token_count} tokens \n
+Debug Langchain conversation: 
+{st.session_state.conversation.memory.buffer}
+""")
+
+components.html("""
+<script>
+const streamlitDoc = window.parent.document;
+
+const buttons = Array.from(
+    streamlitDoc.querySelectorAll('.stButton > button')
+);
+const submitButton = buttons.find(
+    el => el.innerText === 'Submit'
+);
+
+streamlitDoc.addEventListener('keydown', function(e) {
+    switch (e.key) {
+        case 'Enter':
+            submitButton.click();
+            break;
+    }
+});
+</script>
+""", 
+    height=0,
+    width=0,
 )
-openapi_key = st.secrets["OPENAI_API_KEY"]
-
-# Set streamlit page configuration
-st.set_page_config(page_title="AVG Digital Chatbot")
-st.title("AVG Digital Personal Chatbot Assistant")
-
-# Initialize session state variables
-if 'generated' not in st.session_state:
-    st.session_state['generated'] = []  # Store AI generated responses
-
-if 'past' not in st.session_state:
-    st.session_state['past'] = []  # Store past user inputs
-
-if 'entered_prompt' not in st.session_state:
-    st.session_state['entered_prompt'] = ""  # Store the latest user input
-
-# Initialize the ChatOpenAI model
-chat = ChatOpenAI(
-    temperature=0.5,
-    model_name="gpt-3.5-turbo",
-    openai_api_key=openapi_key, 
-    max_tokens=100
-)
-
-
-def build_message_list():
-    """
-    Build a list of messages including system, human and AI messages.
-    """
-    # Start zipped_messages with the SystemMessage
-    zipped_messages = [SystemMessage(
-        # content="You are a helpful AI assistant talking with a human. If you do not know an answer, just say 'I don't know', do not make up an answer.")]
-        content = """You can answer any question user asks from your knowledge."""
-    )]
-
-
-    # Zip together the past and generated messages
-    for human_msg, ai_msg in zip_longest(st.session_state['past'], st.session_state['generated']):
-        if human_msg is not None:
-            zipped_messages.append(HumanMessage(
-                content=human_msg))  # Add user messages
-        if ai_msg is not None:
-            zipped_messages.append(
-                AIMessage(content=ai_msg))  # Add AI messages
-
-    return zipped_messages
-
-
-def generate_response():
-    """
-    Generate AI response using the ChatOpenAI model.
-    """
-    # Build the list of messages
-    zipped_messages = build_message_list()
-
-    # Generate response using the chat model
-    ai_response = chat(zipped_messages)
-
-    return ai_response.content
-
-
-# Define function to submit user input
-def submit():
-    # Set entered_prompt to the current value of prompt_input
-    st.session_state.entered_prompt = st.session_state.prompt_input
-    # Clear prompt_input
-    st.session_state.prompt_input = ""
-
-
-# Create a text input for user
-st.text_input('YOU: ', key='prompt_input', on_change=submit)
-
-
-if st.session_state.entered_prompt != "":
-    # Get user query
-    user_query = st.session_state.entered_prompt
-
-    # Append user query to past queries
-    st.session_state.past.append(user_query)
-
-    # Generate response
-    output = generate_response()
-
-    # Append AI response to generated responses
-    st.session_state.generated.append(output)
-
-
-# Display the chat history
-if st.session_state['generated']:
-    for i in range(len(st.session_state['generated'])-1, -1, -1):
-        # Display AI response
-        message(st.session_state["generated"][i], key=str(i))
-        # Display user message
-        message(st.session_state['past'][i],
-                is_user=True, key=str(i) + '_user')
-
-
-
-hide_streamlit_style = """
-            <style>
-            #MainMenu {visibility: hidden;}
-            footer {visibility: hidden;}
-            header {visibility: hidden;}
-                        div#root a {
-                display: none !important;
-            }
-            </style>
-            """
-st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
