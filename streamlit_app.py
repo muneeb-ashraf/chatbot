9b29b78417050c5b82351978c9a08a09d04e6048
Muneeb Ashraf <58017595+muneebashraf987@users.noreply.github.com>
2023-10-26 04:07:59 +0500

    icon

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import streamlit as st
 
 #from langchain import OpenAI
 from langchain.llms import GooglePalm
-#from langchain.callbacks import get_openai_callback
+from langchain.callbacks import get_openai_callback
 from langchain.chains import ConversationChain
 from langchain.chains.conversation.memory import ConversationSummaryMemory
 import streamlit.components.v1 as components
@@ -37,13 +37,28 @@ def initialize_session_state():
                 google_api_key=st.secrets["PALM_API_KEY"],
                 max_output_tokens=8000,
             )
+     
+
+     
           
         st.session_state.conversation = ConversationChain(
             llm=llm,
             memory=ConversationSummaryMemory(llm=llm),
         )
 
-
+def on_click_callback():
+    with get_openai_callback() as cb:
+        human_prompt = st.session_state.human_prompt
+        llm_response = st.session_state.conversation.run(
+            human_prompt
+        )
+        st.session_state.history.append(
+            Message("human", human_prompt)
+        )
+        st.session_state.history.append(
+            Message("ai", llm_response)
+        )
+        st.session_state.token_count += cb.total_tokens
 
 load_css()
 initialize_session_state()
@@ -59,7 +74,7 @@ with chat_placeholder:
         div = f"""
 <div class="chat-row 
     {'' if chat.origin == 'ai' else 'row-reverse'}">
-    <img class="chat-icon" src="/static/{
+    <img class="chat-icon" src="app/static/{
         'ai_icon.png' if chat.origin == 'ai' 
                       else 'user_icon.png'}"
          width=32 height=32>
